bc31949db5fde3cf1e52cf077669db89c9d64af9
Lakshan Jayawardana <63670833+LakshanJayawardana@users.noreply.github.com>
2021-06-26 02:09:25 +0530

    Add files via upload

---
+++ MoviesAPI/MoviesAPI/Controllers/GenresController.cs
@@ -0,0 +1,85 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using MoviesAPI.DTOs;
+using MoviesAPI.Entities;
+using MoviesAPI.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoviesAPI.Controllers
+{
+    [Route("api/genres")] //Genres Controller so Genres is the controllers
+    [ApiController]
+     public class GenresController: ControllerBase
+    {
+       
+       private readonly ILogger<GenresController> logger;
+        private readonly ApplicationDbContext context;
+        private readonly IMapper mapper;
+
+        public GenresController(ILogger<GenresController> logger,
+            ApplicationDbContext context,IMapper mapper)
+        {
+            this.logger = logger;
+            this.context = context;
+            this.mapper = mapper;
+        }
+
+      
+       // [HttpGet("list")]  //api/genres/list
+
+        [HttpGet] //api/genres
+        public async Task<ActionResult<List<GenreDTO>>> Get()
+        {
+            logger.LogInformation("Getting all the genres");
+
+            var genres = await context.Genres.ToListAsync();
+
+            return mapper.Map<List<GenreDTO>>(genres);
+
+        }
+
+        [HttpGet("{Id:int}", Name = "getGenre")] //api/genres/example
+        public ActionResult<Genre> Get(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        [HttpPost]
+        public async  Task<ActionResult> Post([FromBody] GenreCreationDTO genreCreationDTO)
+        {
+            var genre = mapper.Map<Genre>(genreCreationDTO);
+            context.Add(genre);
+            await context.SaveChangesAsync();
+            return NoContent();
+        }
+
+
+
+        [HttpPut]
+        public ActionResult Put([FromBody] Genre genre)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpDelete]
+        public ActionResult Delete()
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+
+
+
+
+
+    }
+}
---
+++ MoviesAPI/MoviesAPI/Filters/MyExceptionFilter.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoviesAPI.Filters
+{
+    public class MyExceptionFilter : ExceptionFilterAttribute
+    {
+        private readonly ILogger<MyExceptionFilter> logger;
+
+        public MyExceptionFilter(ILogger<MyExceptionFilter> Logger)
+        {
+            this.logger = Logger;
+        }
+
+        public override void OnException(ExceptionContext context)
+        {
+
+            logger.LogError(context.Exception, context.Exception.Message);
+
+            base.OnException(context);
+        }
+
+    }
+}
